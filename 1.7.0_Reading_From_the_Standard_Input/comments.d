0eb0f4d45bda4c3a23bab97bf0ef695c788c07a0
RaddKatt <raddkatt0@gmail.com>
2023-02-22 23:52:40 -0500

    add 1.7.0

---
+++ 1.7.0_Reading_From_the_Standard_Input/comments.d
@@ -0,0 +1,13 @@
+/+
+ // A single line of comment
+
+ /*
+   A comment that spans
+   multiple lines
+ */
+ /+
+   It can even include nested /+ comments +/
+ +/
+
+ A comment block that includes other comments
++/
---
+++ 1.7.0_Reading_From_the_Standard_Input/whitespace_solution.d
@@ -0,0 +1,14 @@
+import std.stdio;
+
+void main() {
+	write("How many students are there? ");
+	int studentCount;
+	readf(" %s", &studentCount);
+
+	write("How many teachers are there? ");
+	int teacherCount;
+	readf(" %s", &teacherCount);
+
+	writeln("Got it: There are ", studentCount, " students",
+			"and ", teacherCount, " teachers.");
+}
